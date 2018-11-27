15eb78494272a48f1e1ec88726d55c87e21b0826
stevenshuang <stevenshuang521@gmail.com>
2018-11-28 00:22:29 +0800

    app.py

---
+++ docker-compose/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from flask import Flask
+from redis import Redis
+
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+
+@app.route('/')
+def index():
+    count = redis.incr('hits')
+    return '<h2>Hello {} views'.format(count)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
